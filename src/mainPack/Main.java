c7ea17fca701a6c5d291392dbe363f8df6585c2f
mpgsa <mpgsa@Sire-Auron-10>
2018-06-02 16:19:49 -0300

    Working on Home Quiz

--- src/mainPack/Main.java
+++ src/mainPack/Main.java
@@ -23,6 +23,7 @@ public class Main
      */
     public static void main(String[] args)
     {
+        hq = new HomeQuiz();
         hq.start();
         
         /*
--- src/questions/HomeQuiz.java
+++ src/questions/HomeQuiz.java
@@ -9,19 +9,57 @@ import java.util.Scanner;
 public class HomeQuiz {
     // Fields
     private String player;
+    private String temp;
+    private int choice;
+    
+    private String MSG_WC = 
+            "\n\n" +
+            "------------------------------------------\n" + 
+            "Welcome to Mega Quiz! \n" +
+            "Enter the player name: ";
+    private String MSG_CH = 
+            "Welcome!\n" +
+            "Choice your Mega Quiz type...\n" +
+            "-- Multiple choice;\n" +
+            "\t 1 - Canada;\n" +
+            "\t 2 - United States\n" +
+            "\t 3 - Country - capital\n" +
+            "4 - Direct response" + 
+            "Your choice:\n";;
     // Objects
     Scanner sc = new Scanner(System.in);
     // Constructor
     public HomeQuiz(){}
     
+    public void choices(){
+        Boolean ok;
+        print(MSG_CH);
+        temp = sc.next();
+        if (ok = check()) {
+            
+        }else{
+            choices();
+        }
+    }
+    
     public void start(){
-        System.out.println(
-            "Welcome to Mega Quiz! \n" +
-            "Enter the player name: \n"
-        );
+        print(MSG_WC);
         player = sc.next();
-        
-        System.out.println(player);
+        choices();
+    }
+    
+    private void print(String text){
+        System.out.println(text);
+    }
+    
+    private boolean check(){
+        try {
+            choice = Integer.parseInt(temp);
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
     }
     
 }
